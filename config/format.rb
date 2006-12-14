234e6cf3fa212d02976f3f88cef5ca4ad566c6ae
Suraj N. Kurapati <sunaku@gmail.com>
2006-12-14 00:33:11 -0800

    add preserving of content for <code> and <tt> tags
    fix indented text in Textile (<pre><code>...</code></pre>) is now converted into <pre>...</pre>

--- config/format.rb
+++ config/format.rb
@@ -36,9 +36,33 @@ require 'coderay'
 require 'redcloth'
 
 class String
+  # The content of these HTML tags will be preserved verbatim when they are processed by Textile.
+  PRESERVED_TAGS = [:code, :tt]
+
   # Transforms this string into HTML.
   def to_html
-    redcloth.coderay
+    text = dup
+
+    # prevent the content of these tags from being transformed by Textile
+    # for example, Textile transforms quotation marks in code into curly ones (&#8192;) -- this ruins any source code in the content of the tags!
+      PRESERVED_TAGS.each do |tag|
+        text.gsub! %r{<#{tag}(.*?)>(.*?)</#{tag}>}m, %{<pre tag=#{tag.inspect} \\1>\\2</pre>}
+      end
+
+    html = text.redcloth
+
+    # restore the original tags for the preserved content
+      PRESERVED_TAGS.each do |tag|
+        html.gsub! %r{<pre tag=#{tag.inspect}(.*?)>(.*?)</pre>}m, %{<#{tag}\\1>\\2</#{tag}>}
+      end
+
+    # reject indented text in Textile as possible source code
+    # also, HTML escape the indented content in case it has HTML
+      html.gsub! %r{(<pre>)<code>(.*?)\s*</code>(</pre>)}m do
+        $1 + CGI.escapeHTML($2) + $3
+      end
+
+    html.coderay
   end
 
   # Returns the result of running this string through RedCloth.
@@ -46,7 +70,7 @@ class String
     RedCloth.new(self).to_html
   end
 
-  # Adds syntax coloring to <code>...</code> elements in the given text. If
+  # Adds syntax coloring to <code> elements in the given text. If
   # the <code> tag has an attribute lang="...", then that is considered the
   # programming language for which appropriate syntax coloring should be
   # applied. Otherwise, the programming language is assumed to be ruby.
