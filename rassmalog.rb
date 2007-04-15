7c3e58f8fc50b681004ef5eee6f4e53b301c1f1f
Suraj N. Kurapati <sunaku@gmail.com>
2007-04-15 15:19:18 -0700

    change from GPL to Sleepycat+MIT hybrid license

--- rassmalog.rb
+++ rassmalog.rb
@@ -1,22 +1,7 @@
-=begin
-  Copyright 2006 Suraj N. Kurapati
-
-  This file is part of Rassmalog.
-
-  Rassmalog is free software; you can redistribute it and/or
-  modify it under the terms of the GNU General Public License
-  as published by the Free Software Foundation; either version 2
-  of the License, or (at your option) any later version.
-
-  Rassmalog is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with Rassmalog; if not, write to the Free Software Foundation,
-  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-=end
+# The main source code of Rassmalog.
+#--
+# Copyright 2006-2007 Suraj N. Kurapati
+# See the file named LICENSE for details.
 
 require 'rake/clean'
 require 'config/format'
